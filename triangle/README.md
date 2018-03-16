e5e0af261e5399923dedee7590b202718b020333
taba <hgKaguyaToupako4er@gmail.com>
2018-03-16 23:00:28 +0900

    Add triangle

---
+++ triangle/README.md
@@ -0,0 +1,2 @@
+# refference
+https://www.youtube.com/watch?v=T2Hwu-XiVkA
---
+++ triangle/src/main.rs
@@ -0,0 +1,49 @@
+//!  sierpinski triangle image generator
+
+extern crate image;
+extern crate rand;
+
+use rand::Rng;
+use std::fs::File;
+use std::path::Path;
+
+pub struct Point {
+    x: u32,
+    y: u32,
+}
+
+const WIDTH:  u32 = 800;
+const HEIGHT: u32 = 600;
+
+/// main program
+fn main() {
+    let mut img = image::ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
+        if x == 0 && y == 0 {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+
+    let mut cnt: u32 = 1000;
+    let pts: [Point; 3] = [
+        Point {x: WIDTH / 2, y: 0},
+        Point {x: 0, y: HEIGHT},
+        Point {x: WIDTH, y: HEIGHT}
+    ];
+
+    let mut p = Point { x: 350, y: 350 };
+    let pixel = img[(0, 0)];
+
+    while cnt > 0 {
+        cnt = cnt - 1;
+        let num = rand::thread_rng().gen_range(0,3);
+        p.x = (p.x + pts[num].x) / 2;
+        p.y = (p.y + pts[num].y) / 2;
+        img.put_pixel(p.x, p.y, pixel);
+    }
+
+    let ref mut font = File::create(&Path::new("tri.png")).unwrap();
+    let _ = image::ImageLuma8(img).save(font, image::PNG);
+
+}
