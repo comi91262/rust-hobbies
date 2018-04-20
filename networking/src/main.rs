216c821168b9e1999b729162b30f7eef04966a43
taba <hgKaguyaToupako4er@gmail.com>
2018-04-20 23:01:49 +0900

    Add networking

---
+++ networking/src/main.rs
@@ -0,0 +1,16 @@
+extern crate url;
+
+use url::Url;
+
+fn run() -> Result<()> {
+    let s = "https://github.com/rust-lang/rust/issues?labels=E-easy&state=open";
+
+    let parsed = Url::parse(s)?;
+    println!("The path part of the URL is: {}", parsed.path());
+
+    Ok(())
+}
+
+
+fn main() {
+}
