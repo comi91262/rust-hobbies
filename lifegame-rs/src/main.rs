58e05a482e43be318a0a21232c8dd5294352066d
taba <hgKaguyaToupako4er@gmail.com>
2018-05-20 23:32:34 +0900

    Update lifegame-rs

--- lifegame-rs/src/main.rs
+++ lifegame-rs/src/main.rs
@@ -1,3 +1,19 @@
+extern crate glium;
+
 fn main() {
-    println!("Hello, world!");
+    // 1. The **winit::EventsLoop** for handling events.
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    // 2. Parameters for building the Window.
+    let window = glium::glutin::WindowBuilder::new()
+        .with_dimensions(1024, 768)
+        .with_title("Hello world");
+    // 3. Parameters for building the OpenGL context.
+    let context = glium::glutin::ContextBuilder::new();
+    // 4. Build the Display with the given window and OpenGL context parameters and register the
+    //    window with the events_loop.
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    let mut frame = display.draw();
+    loop {
+    }
 }
