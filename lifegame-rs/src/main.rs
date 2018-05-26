9f7bf019d91e800337171a05cffe020dc664550d
taba <hgKaguyaToupako4er@gmail.com>
2018-05-26 23:19:32 +0900

    Update

--- lifegame-rs/src/main.rs
+++ lifegame-rs/src/main.rs
@@ -1,3 +1,73 @@
+//#[macro_use]
+//extern crate glium;
+//
+//fn main() {
+//    use glium::{glutin, Surface};
+//
+//    let mut events_loop = glutin::EventsLoop::new();
+//    let window = glutin::WindowBuilder::new();
+//    let context = glutin::ContextBuilder::new();
+//    let display = glium::Display::new(window, context, &events_loop).unwrap();
+//
+//    #[derive(Copy, Clone)]
+//    struct Vertex {
+//        position: [f32; 2],
+//    }
+//
+//    implement_vertex!(Vertex, position);
+//
+//    let vertex1 = Vertex { position: [-0.5, -0.5] };
+//    let vertex2 = Vertex { position: [ 0.5, -0.5] };
+//    let vertex3 = Vertex { position: [-0.5,  0.5] };
+//    let vertex4 = Vertex { position: [ 0.5,  0.5] };
+//    let shape1 = vec![vertex1, vertex2, vertex4];
+//    let shape2 = vec![vertex1, vertex3, vertex4];
+//
+//    let vertex_buffer1 = glium::VertexBuffer::new(&display, &shape1).unwrap();
+//    let vertex_buffer2 = glium::VertexBuffer::new(&display, &shape2).unwrap();
+//
+//    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+//
+//
+//    let vertex_shader_src = r#"
+//        #version 140
+//        in vec2 position;
+//        void main() {
+//            gl_Position = vec4(position, 0.0, 1.0);
+//        }
+//    "#;
+//
+//    let fragment_shader_src = r#"
+//        #version 140
+//        out vec4 color;
+//        void main() {
+//            color = vec4(0.0, 1.0, 0.0, 1.0);
+//        }
+//    "#;
+//
+//    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+//
+//    let mut closed = false;
+//    while !closed {
+//        let mut target = display.draw();
+//        target.clear_color(0.0, 0.0, 0.0, 1.0);
+//        target.draw(&vertex_buffer1, &indices, &program, &glium::uniforms::EmptyUniforms,
+//                    &Default::default()).unwrap();
+//        target.draw(&vertex_buffer2, &indices, &program, &glium::uniforms::EmptyUniforms,
+//                    &Default::default()).unwrap();
+//        target.finish().unwrap();
+//
+//        events_loop.poll_events(|event| {
+//            match event {
+//                glutin::Event::WindowEvent { event, .. } => match event {
+//                    glutin::WindowEvent::Closed => closed = true,
+//                    _ => ()
+//                },
+//                _ => (),
+//            }
+//        });
+//    }
+//}
 #[macro_use]
 extern crate glium;
 
@@ -17,23 +87,19 @@ fn main() {
     implement_vertex!(Vertex, position);
 
     let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.5, -0.5] };
-    let vertex3 = Vertex { position: [-0.5,  0.5] };
-    let vertex4 = Vertex { position: [ 0.5,  0.5] };
-    let shape1 = vec![vertex1, vertex2, vertex4];
-    let shape2 = vec![vertex1, vertex3, vertex4];
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
 
-    let vertex_buffer1 = glium::VertexBuffer::new(&display, &shape1).unwrap();
-    let vertex_buffer2 = glium::VertexBuffer::new(&display, &shape2).unwrap();
-
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
 
     let vertex_shader_src = r#"
         #version 140
         in vec2 position;
+        uniform mat4 matrix;
         void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
 
@@ -41,19 +107,34 @@ fn main() {
         #version 140
         out vec4 color;
         void main() {
-            color = vec4(0.0, 1.0, 0.0, 1.0);
+            color = vec4(1.0, 0.0, 0.0, 1.0);
         }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
+    let mut t: f32 = -0.5;
     let mut closed = false;
     while !closed {
+        // we update `t`
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        target.draw(&vertex_buffer1, &indices, &program, &glium::uniforms::EmptyUniforms,
-                    &Default::default()).unwrap();
-        target.draw(&vertex_buffer2, &indices, &program, &glium::uniforms::EmptyUniforms,
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [ t , 0.0, 0.0, 1.0f32],
+            ]
+        };
+
+        target.draw(&vertex_buffer, &indices, &program, &uniforms,
                     &Default::default()).unwrap();
         target.finish().unwrap();
 
