2d09220c04ca681ad5b4dae561ea7ff2dde50a73
ykonomi <hgKaguyaToupako4er@gmail.com>
2018-10-04 04:21:50 +0900

    Update triangle-rs.

--- triangle/README.md
+++ triangle/README.md
@@ -1,2 +1,2 @@
-# refference
-https://www.youtube.com/watch?v=T2Hwu-XiVkA
+# Reference
+[rust lang 001- Sierpinski Triangle Image Generator](https://www.youtube.com/watch?v=T2Hwu-XiVkA)
--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -12,7 +12,7 @@ pub struct Point {
     y: u32,
 }
 
-const WIDTH:  u32 = 800;
+const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
 /// main program
@@ -27,9 +27,12 @@ fn main() {
 
     let mut cnt: u32 = 1000;
     let pts: [Point; 3] = [
-        Point {x: WIDTH / 2, y: 0},
-        Point {x: 0, y: HEIGHT},
-        Point {x: WIDTH, y: HEIGHT}
+        Point { x: WIDTH / 2, y: 0 },
+        Point { x: 0, y: HEIGHT },
+        Point {
+            x: WIDTH,
+            y: HEIGHT,
+        },
     ];
 
     let mut p = Point { x: 350, y: 350 };
@@ -37,7 +40,7 @@ fn main() {
 
     while cnt > 0 {
         cnt = cnt - 1;
-        let num = rand::thread_rng().gen_range(0,3);
+        let num = rand::thread_rng().gen_range(0, 3);
         p.x = (p.x + pts[num].x) / 2;
         p.y = (p.y + pts[num].y) / 2;
         img.put_pixel(p.x, p.y, pixel);
@@ -45,5 +48,4 @@ fn main() {
 
     let ref mut font = File::create(&Path::new("tri.png")).unwrap();
     let _ = image::ImageLuma8(img).save(font, image::PNG);
-
 }
